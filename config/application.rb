da8b3ee4f95d4ada0d5549223bf518eb270b933e
Bruno <bruno@feres.dev>
2020-11-22 09:40:07 -0300

    Initial Commit

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require "rails"
+
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+
+Bundler.require(*Rails.groups)
+
+module EcommerceApi
+  class Application < Rails::Application
+    config.load_defaults 6.0
+
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+
+    config.api_only = true
+
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
+  end
+end
